c9ff4163ee68df22e7c72192144d4f05ea1a4757
yulong <yulong@huayun.com>
2021-05-22 14:50:49 +0800

    chore: 使用normalize 初始化css，配置eslint规则

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+// 初始化css
+import 'normalize.css/normalize.css'
 
 const app = createApp(App)
 
