ad6f9571c88b2d60d7f9046b135f179fd6e04b3c
yulong <yulong@huayun.com>
2021-05-22 15:31:39 +0800

    feat: layout, dashbord, style样式文件

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import store from './store'
 import installElementPlus from './plugins/element'
 // 初始化css
 import 'normalize.css/normalize.css'
+// 全局 css
+import '@/styles/index.scss'
 
 const app = createApp(App)
 
 app
   .use(store)
   .use(router)
-  .use(installElementPlus)
+  // .use(installElementPlus)
   .mount('#app')
